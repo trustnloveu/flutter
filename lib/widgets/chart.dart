9ad145dce1fb09c70d350bc31e70590f21f5c017
austin <trustnloveu@gmail.com>
2021-04-04 16:39:39 +0900

    Place chart bars in an organized order using FiitedBox, SizedBox, FlexFit. Give some padding as well

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,3 +1,4 @@
+import 'package:expense_planner/widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -29,6 +30,12 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get totalSpending {
+    return groupedTranscationValues.fold(0.0, (sum, item) {
+      return sum += item['amount'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // print(groupedTranscationValues);
@@ -37,12 +44,27 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: [
-          ...groupedTranscationValues.map((data) {
-            return Text('${data['day']} : ${data['amount'].toString()}');
-          }),
-        ],
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ...groupedTranscationValues.map((data) {
+              // return Text('${data['day']} : ${data['amount'].toString()}');
+              return Flexible(
+                // flex: 1,
+                fit: FlexFit.tight, // loose
+                child: ChartBar(
+                  data['day'],
+                  data['amount'],
+                  totalSpending == 0.0
+                      ? 0.0
+                      : (data['amount'] as double) / totalSpending,
+                ),
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
