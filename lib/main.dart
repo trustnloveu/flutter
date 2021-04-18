2d1b572b58d213a8fdf0a75d602f9f6106ebea7b
euijin-yang <trustnloveu@gmail.com>
2021-04-18 18:49:53 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage();
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // build
+  @override
+  Widget build(BuildContext context) {
+    // return
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('123'),
+      ),
+      body: Center(
+        child: Text('123'),
+      ),
+    );
+  }
+}
