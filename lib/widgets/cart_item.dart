f8e3ac0ea4d93d51c0927c616e635c3f9a502fb9
euijin-yang <trustnloveu@gmail.com>
2021-04-26 02:25:57 +0900

    Snackbar including additional options & dismiss options with alert messages

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -22,10 +22,9 @@ class CartItem extends StatelessWidget {
   // build
   @override
   Widget build(BuildContext context) {
-    // return
+    // return > Dismissible
     return Dismissible(
       key: ValueKey(id),
-      direction: DismissDirection.endToStart,
       background: Container(
         color: Theme.of(context).errorColor,
         alignment: Alignment.centerRight,
@@ -37,9 +36,41 @@ class CartItem extends StatelessWidget {
           size: 40,
         ),
       ),
+
+      // direction 
+      direction: DismissDirection.endToStart,
+
+      // onDismissed
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
+
+      // confirmDismiss
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove the item from the cart?'),
+            actions: [
+              TextButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+              ),
+              TextButton(
+                child: Text('Yes'),
+                onPressed: () {
+                   Navigator.of(ctx).pop(true);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+
+      // child
       child: Card(
         margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         child: Padding(
