d61564a24521e675d592de990424c44f58e20074
euijin-yang <ejyang@upchain.kr>
2021-04-30 22:46:09 +0900

    Send Http POST request to Firebase

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 // Models
 import 'package:shop_app/providers/product.dart';
@@ -55,6 +57,21 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    final url = Uri.parse(
+        'https://flutter-shop-app-e61d9-default-rtdb.asia-southeast1.firebasedatabase.app/products.json');
+    http.post(
+      url,
+      body: json.encode(
+        {
+          'title': product.title,
+          'description': product.description,
+          'imageUrl': product.imageUrl,
+          'price': product.price,
+          'isFavorite': product.isFavorite,
+        },
+      ),
+    );
+
     final newProduct = Product(
       id: DateTime.now().toString(),
       title: product.title,
