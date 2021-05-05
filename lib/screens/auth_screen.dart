7c2d9672d7fab97ee304a201fae50a234acd8637
euijin-yang <trustnloveu@gmail.com>
2021-05-06 01:42:05 +0900

    Switch AnimatedBuilder to AnimatedContainer

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -263,17 +263,16 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-            // height: _authMode == AuthMode.Signup ? 320 : 260,
-            height: _heightAnimation.value.height,
-            constraints:
-                // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-                BoxConstraints(minHeight: _heightAnimation.value.height),
-            width: deviceSize.width * 0.75,
-            padding: EdgeInsets.all(16.0),
-            child: ch),
+      child: AnimatedContainer(
+        height: _authMode == AuthMode.Signup ? 320 : 260, // for AnimatedContainer
+        duration: Duration(milliseconds: 300), // for AnimatedContainer
+        curve: Curves.easeIn, // for AnimatedContainer
+        // height: _heightAnimation.value.height, // used in AnimatedBuilder & AnimationController
+        constraints:
+            // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260), // used in AnimatedBuilder & AnimationController
+            BoxConstraints(minHeight: _heightAnimation.value.height),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
