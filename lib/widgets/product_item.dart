e6c3f96f6dcd896e9cab39d289a38c0d33e9bf58
euijin-yang <trustnloveu@gmail.com>
2021-05-06 02:08:43 +0900

    Work with images for animation using Hero, FadeInImage widgets

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -30,9 +30,13 @@ class ProductItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(7.5),
       child: GridTile(
         child: GestureDetector(
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
           onTap: () {
             Navigator.of(context).pushNamed(
