e8272c09583d72ff1b376f465de553fa5a52e363
euijin-yang <trustnloveu@gmail.com>
2021-05-08 11:12:31 +0900

    Change Order List to animating view depending on expanding status

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -24,31 +24,38 @@ class _OrderItemState extends State<OrderItem> {
   @override
   Widget build(BuildContext context) {
     // return
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('\$${widget.order.amount}'),
-            subtitle: Text(
-              DateFormat('yyyy-MM-dd HH:mm').format(widget.order.dateTime),
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height:
+          _expanded ? min(widget.order.products.length * 20.0 + 200, 200) : 100,
+      child: Card(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('\$${widget.order.amount}'),
+              subtitle: Text(
+                DateFormat('yyyy-MM-dd HH:mm').format(widget.order.dateTime),
+              ),
+              // Expanding Order Detail Event
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-            // Expanding Order Detail Event
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
 
-          // Expanded View of Order Detail
-          if (_expanded)
-            Container(
+            // Expanded View of Order Detail
+            // if (_expanded) //* comment out when you replace AnimatedContainer with Container
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20.0 + 30, 100),
+              height: _expanded
+                  ? min(widget.order.products.length * 20.0 + 30, 100)
+                  : 0,
               child: ListView(
                 children: <Widget>[
                   ...widget.order.products.map(
@@ -75,7 +82,8 @@ class _OrderItemState extends State<OrderItem> {
                 ],
               ),
             ),
-        ],
+          ],
+        ),
       ),
     );
   }
